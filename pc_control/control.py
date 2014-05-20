53c23d8dedcf08eece4bf4feb94a724768b0ac74
Phyks <webmaster@phyks.me>
2014-05-21 01:01:25 +0200

    Control class

--- pc_control/control.py
+++ pc_control/control.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python2
 # -*- coding: utf8 -*-
 
-# Parameters
-corrections = {'r': 1.0, 'g': 1.0, 'b': 1.0}
-brightness = 1.0
-# ==========
-
-corrections = {k: brightness * corrections[k] for k in corrections}
+# All colors are in [0,255] but we have to send values between [0,127]
 
 
-def get_corrected_color(color):
-    """Corrects the color to match real LED render
+class Control():
+    def __init__(self, ser, nb_leds,
+                 corrections={'r': 1.0, 'g': 1.0, 'b': 1.0},
+                 brightness=1.0):
+        self.corrections = {k: brightness * corrections[k]
+                            for k in corrections}
+        self.ser = ser
+        self.nb_leds = nb_leds
+        self.current_colors = [{'r': 255, 'g': 0, 'b': 0}
+                               for i in range(nb_leds)]
 
-    params: color a {r, g, b} dict
-    returns: a color dict
-    """
-    return {i: color[i] * corrections[i] for i in color}
+    def get_current_colors(self, id=None):
+        if id is None:
+            return self.current_colors
+        else:
+            return self.current_colors[id]
 
+    def correct_color(self, color):
+        """Corrects the color to match real LED render
 
-def send_color(id, color, ser):
-    """Corrects the RGB127 color and send it over serial"""
-    data = [0x80 + id]
-    data.extend(v for k, v in get_corrected_color(color).iteritems())
-    for i in data:
-        ser.write(chr(i))
+        params: color a {r, g, b} dict
+        returns: a color dict
+        """
+        return {i: color[i] * self.corrections[i] >> 1 for i in color}
+
+    def send_color(self, id, color):
+        """Corrects the color and send it over serial"""
+        data = [0x80 + id]
+        data.extend(v for k, v in self.correct_color(color).iteritems())
+        for i in data:
+            self.ser.write(chr(i))
+        self.current_color[id] = color
+
+    def send_colors(self, colors):
+        """Send the colors to all the LEDs"""
+        for i in range(min(self.nb_leds,len(colors)-1)):
+            self.send_color(i, colors[i])
+
+    def reset(self):
+        """Resets all the LEDs to red"""
+        for k in range(self.nb_leds):
+            for i in [0x80 + k, 127, 0, 0]:
+                self.ser.write(chr(i))
